98ca23121b390f6e4bc50626a3f6468e6ffb1e33
jjignacio <you@example.com>
2020-09-23 20:49:07 -0300

    Merge Header and Table

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React, {Component}  from 'react';
 import { BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import './App.css';
 import Login from './components/Login'
-<<<<<<< Updated upstream
-import Empresa from './components/Empresa/Company'
-
-=======
 import Empresa from './components/empresa/Company'
 import Encuesta from './components/empresa/Survey'
 import Profile from './components/empresa/Profile'
->>>>>>> Stashed changes
 import ObservatorioPyme from './components/observatorioPyme/MainView'
 
 class App extends Component {
